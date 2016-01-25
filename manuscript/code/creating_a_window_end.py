67bf9b4ce595554a3a2d3c63ee5a07be0ef18ab8
Martin Fitzpatrick <martin.fitzpatrick@gmail.com>
2016-01-25 22:25:32 +0100

    Fix tables

--- manuscript/code/creating_a_window_end.py
+++ manuscript/code/creating_a_window_end.py
@@ -2,7 +2,6 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 
-# Only needed for access to command line arguments
 import sys
 
 
@@ -25,21 +24,12 @@ class MainWindow(QMainWindow):
         self.setCentralWidget(label)
 
 
-# You need one (and only one) QApplication instance per application.
-# Pass in sys.argv to allow command line arguments for your app.
-# If you know you won't use command line arguments QApplication([]) works too.
 app = QApplication(sys.argv)
 
 window = MainWindow()
-window.show() # IMPORTANT!!!!! Windows are hidden by default.
+window.show()
 
-# Start the event loop.
 app.exec_()
-
-
-# Your application won't reach here until you exit and the event 
-# loop has stopped.
-
 
 
 
