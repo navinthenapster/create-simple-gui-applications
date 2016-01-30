59b8a5c749efef07991069dbac3dd3836fa5da33
Martin Fitzpatrick <martin.fitzpatrick@gmail.com>
2016-01-30 17:03:48 +0100

    QAction

--- manuscript/code/toolbars_and_menus_5.py
+++ manuscript/code/toolbars_and_menus_5.py
@@ -6,21 +6,15 @@ class MainWindow(QMainWindow):
         self.setWindowTitle("My Awesome App")
         
         label = QLabel("THIS IS AWESOME!!!")
-
-        # The `Qt` namespace has a lot of attributes to customise
-        # widgets. See: http://doc.qt.io/qt-5/qt.html
         label.setAlignment(Qt.AlignCenter)
         
-        # Set the central widget of the Window. Widget will expand
-        # to take up all the space in the window by default.
         self.setCentralWidget(label)
         
         toolbar = QToolBar("My main toolbar")
         toolbar.setIconSize(QSize(16,16))
         self.addToolBar(toolbar)
         
-        
-        button_action = QAction(QIcon("bug.png"), "&Your button", self)
+        button_action = QAction(QIcon("bug.png"), "Your button", self)
         button_action.setStatusTip("This is your button")
         button_action.triggered.connect(self.onMyToolBarButtonClick)
         button_action.setCheckable(True)
@@ -28,7 +22,7 @@ class MainWindow(QMainWindow):
         
         toolbar.addSeparator()
         
-        button_action2 = QAction(QIcon("bug.png"), "Your &button2", self)
+        button_action2 = QAction(QIcon("bug.png"), "Your button2", self)
         button_action2.setStatusTip("This is your button2")
         button_action2.triggered.connect(self.onMyToolBarButtonClick)
         button_action2.setCheckable(True)
@@ -40,16 +34,6 @@ class MainWindow(QMainWindow):
         self.setStatusBar(QStatusBar(self))
         
         
-        menu = self.menuBar()
-        menu.setNativeMenuBar(False) # Disables the global menu bar on MacOS
-        
-        file_menu = menu.addMenu("&File")
-        file_menu.addAction(button_action)
-        
-        file_menu.addSeparator()
-        
-        file_menu.addAction(button_action2)
-        
-        
     def onMyToolBarButtonClick(self, s):
         print("click", s)
+
